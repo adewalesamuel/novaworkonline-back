ac1e9c503682b347e6331b82ed023b2e9f7ae6d5
adewalesamuel <samroberval@gmail.com>
2023-12-26 22:31:45 +0000

    bugfix migatioin file test table

--- database/migrations/2023_08_22_1692718041_create_usertests_table.php
+++ database/migrations/2023_08_22_1692718041_create_usertests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Createuser_testsTable extends Migration
+class CreateUserTestsTable extends Migration
 {
     /**
      * Run the migrations.
